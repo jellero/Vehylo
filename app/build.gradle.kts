ba76ebbb494a76f15527cc96603653a1112802b2
jellero <j.ellero@gmail.com>
2026-08-04 10:34:40 +0200

    Bump Vehylo to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.jellero.vehylo"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
