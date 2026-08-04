e289439b2d3907ffcc734b8c93bdecb4c4fbb734
jellero <j.ellero@gmail.com>
2026-08-04 10:40:23 +0200

    Target stable Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "io.github.jellero.vehylo"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.jellero.vehylo"
         minSdk = 26
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 2
         versionName = "0.2.0"
 
