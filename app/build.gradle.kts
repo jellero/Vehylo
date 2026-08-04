9fadc5cf7b21ce4a41332c030263c5c113e85118
jellero <j.ellero@gmail.com>
2026-08-04 11:12:54 +0200

    Use Lifecycle 2.10 with Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2026.06.00"))
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.11.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
